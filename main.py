653d44ae32d800830d57c6b58db9d6bde3dfa625
PetrikDima <108899164+PetrikDima@users.noreply.github.com>
2022-09-03 18:35:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
 from datetime import datetime
-import decorators
 from collections import UserDict
 
 
@@ -94,18 +92,51 @@ class AddressBook(UserDict):
     def add_record(self, record: Record):
         self.data[record.name.value] = record
 
-    def iterator(self):
-        pass
+    def iterator(self, max_count):
+        count = 0
+        for item in self.data:
+            if count < max_count:
+                count += 1
+                yield self.data[item]
+        else:
+            raise StopIteration
 
 
 address_book = AddressBook()
 
 
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IndexError:
+            return """If you write command 'add' please write 'add' 'name' 'number'
+If you write command 'change' please write 'change' 'name' 'number'
+If you write command 'phone' please write 'phone' 'name'
+If you write command 'remove' please write 'remove' 'name' 'number'
+If you write command 'records' please write 'records' and number of records"""
+        except KeyError:
+            return "This key not found try again"
+        except TypeError:
+            return "Type not supported try again"
+        except ValueError:
+            return "Incorrect value try again"
+        except AttributeError:
+            return "Incorrect attribute try again"
+        except StopIteration:
+            return "That's all"
+        except RuntimeError:
+            return "..."
+
+    return wrapper
+
+
+@input_error
 def hello_handler():
     return 'How can I help you?'
 
 
-@decorators.add_error_decorator
+@input_error
 def add_handler(*args):
     name = Name(args[0])
     phone = Phone(args[1])
@@ -119,7 +150,7 @@ def add_handler(*args):
     return 'Telephone number has beem added'
 
 
-@decorators.change_error_decorator
+@input_error
 def change_handler(*args):
     old_phone = Phone(args[1])
     phone = Phone(args[2])
@@ -128,32 +159,40 @@ def change_handler(*args):
         return 'Telephone number has beem changed'
 
 
-@decorators.phone_error_decorator
+@input_error
 def phone_handler(*args):
     return address_book[args[0]]
 
 
+@input_error
 def show_all_handler():
-    return "\n".join((f"{values} " for values in address_book.values()))
+    return "\n".join([f"{v} " for v in address_book.values()])
 
 
+@input_error
+def show_n_handler(*args):
+    for item in address_book.iterator(int(args[0])):
+        print(item)
+
+
+@input_error
 def days_to_birthday_handler(*args):
     record = address_book[args[0]]
-    s_record = (repr(record))
-    date = re.search(r'\d{2}\.\d{2}\.\d{4}', s_record)
-    birthday = Birthday(date.group())
-    result = record.days_to_birthday(birthday)
+    result = record.days_to_birthday()
     return result
 
 
+@input_error
 def exit1_handler():
     return 'Good bye!'
 
 
+@input_error
 def exit2_handler():
     return 'Good bye!'
 
 
+@input_error
 def exit3_handler():
     return 'Good bye!'
 
@@ -165,6 +204,7 @@ commands = {
     phone_handler: 'phone',
     show_all_handler: 'show all',
     days_to_birthday_handler: 'birthday',
+    show_n_handler: 'records',
     exit1_handler: 'exit',
     exit2_handler: 'close',
     exit3_handler: 'good bye',
@@ -183,7 +223,7 @@ def user_input_parser(user_input):
     return command, data
 
 
-@decorators.input_error_decorator
+@input_error
 def main():
     while True:
         user_input = input('Command: ').lower()
@@ -196,3 +236,6 @@ def main():
 if __name__ == '__main__':
     main()
 # add dima +380975323736 25.05.2004
+# add bob +380653432716 27.12.2003
+# add max +380725679728 1.11.2002
+# add ann +380984322716 12.6.2006
